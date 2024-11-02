f9ac8e318806ab2f1bd2f44174e6a2640fc9e587
Dandraghas <dandraghas@gmail.com>
2024-11-02 13:30:37 +0200

    i18n: add a few locales

--- system/core/default.nix
+++ system/core/default.nix
@@ -12,6 +12,9 @@
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [
       "en_US.UTF-8/UTF-8"
+      "ru_RU.UTF-8/UTF-8"
+      "uk_UA.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
     ];
   };
 
